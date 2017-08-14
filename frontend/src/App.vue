8558cefe572b69b7ae52fec2986a898de45afd7d
xugang <hubeixugang@163.com>
2017-08-14 23:52:57 +0800

    添加静态资源

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+	<!-- element-ui navMenu -->
+	<el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" :router=true>
+	  <el-menu-item index="home">RestBoard</el-menu-item>
+	  <el-menu-item index="app">
+	    App
+	  </el-menu-item>
+	  <el-menu-item index="api">Api</el-menu-item>
+	  <el-menu-item index="request">Request</el-menu-item>
+	  <el-menu-item index="doc">Doc</el-menu-item>
+	</el-menu>
+	<!-- 占位符 -->
+	<router-view></router-view>
+	
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeIndex: 'home'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/doc/doc.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        doc
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
